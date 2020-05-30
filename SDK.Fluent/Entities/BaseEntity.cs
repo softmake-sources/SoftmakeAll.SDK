277c297234123109f25cb522449d073f56d35816
Arthur Mattos <arsmmd@hotmail.com>
2020-05-30 03:28:13 -0300

    Starting the Fluents projects

---
+++ SDK.Fluent/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace SoftmakeAllSDK.Fluent
+{
+  public abstract class BaseEntity
+  {
+    #region Constructor
+    public BaseEntity() { }
+    #endregion
+
+    #region Properties
+    public System.String ID { get; set; }
+    public System.Guid UniqueID { get; set; }
+    public System.String RowVersion { get; set; }
+    public System.DateTimeOffset Inserted { get; set; }
+    public System.Nullable<System.DateTimeOffset> Updated { get; set; }
+    public System.Text.Json.JsonElement JSONData { get; set; }
+    #endregion
+  }
+}
