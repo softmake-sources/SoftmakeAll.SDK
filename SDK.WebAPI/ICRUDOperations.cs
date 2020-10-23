e6998f7f91b4e1335dbc8b74ae1df6c38a8e248b
Arthur Mattos <arsmmd@hotmail.com>
2020-10-23 18:30:14 -0300

    Changing Root Namespace for some projects and including the 2 interfaces in SDK.WebAPI project.

---
+++ SDK.WebAPI/ICRUDOperations.cs
@@ -0,0 +1,15 @@
+﻿namespace SoftmakeAll.SDK.WebAPI
+{
+  public interface ICRUDOperations
+  {
+    #region Methods
+    public System.Threading.Tasks.Task<SoftmakeAll.SDK.OperationResult<System.Text.Json.JsonElement>> CreateAsync(System.Text.Json.JsonElement Object);
+    public System.Threading.Tasks.Task<SoftmakeAll.SDK.OperationResult<System.Text.Json.JsonElement>> ReadAsync(System.String ID);
+    public System.Threading.Tasks.Task<SoftmakeAll.SDK.OperationResult<System.Text.Json.JsonElement>> ReadAsync(System.String Fields, System.String Filter, System.String Group, System.String Sort, System.Int32 Skip, System.Int32 Take);
+    public System.Threading.Tasks.Task<SoftmakeAll.SDK.OperationResult<System.Text.Json.JsonElement>> UpdatePartialAsync(System.String ID, System.Text.Json.JsonElement Object);
+    public System.Threading.Tasks.Task<SoftmakeAll.SDK.OperationResult<System.Text.Json.JsonElement>> UpdateAsync(System.String ID, System.Text.Json.JsonElement Object);
+    public System.Threading.Tasks.Task<SoftmakeAll.SDK.OperationResult<System.Text.Json.JsonElement>> DeleteMethodAsync(System.String ID);
+    public System.Threading.Tasks.Task<SoftmakeAll.SDK.OperationResult<System.Text.Json.JsonElement>> DeleteMethodAsync(System.Text.Json.JsonElement Array);
+    #endregion
+  }
+}
---
+++ SDK.WebAPI/INotificationEmitter.cs
@@ -0,0 +1,9 @@
+﻿namespace SoftmakeAll.SDK.WebAPI
+{
+  public interface INotificationEmitter
+  {
+    #region Methods
+    public void Raise(System.Text.Json.JsonElement Body);
+    #endregion
+  }
+}
