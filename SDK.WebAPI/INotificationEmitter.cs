cef1d612066975803fc754d21a2119e91727272b
Arthur Mattos <arsmmd@hotmail.com>
2020-12-19 14:33:01 -0300

    INotificationEmitter.cs - WebAPI

--- SDK.WebAPI/INotificationEmitter.cs
+++ SDK.WebAPI/INotificationEmitter.cs
@@ -3,6 +3,9 @@
   public interface INotificationEmitter
   {
     #region Methods
+    public void WebSocketNotify(System.Text.Json.JsonElement Body);
+    public void WebSocketNotify(System.Text.Json.JsonElement Body, System.String Method);
+    public void WebSocketNotify(System.Text.Json.JsonElement Body, System.String Method, System.Collections.Generic.List<System.String> Usernames);
     public void Raise(System.Text.Json.JsonElement Body);
     #endregion
   }
