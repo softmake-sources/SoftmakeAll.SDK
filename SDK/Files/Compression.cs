592da6aa2656d63984f435b17d8a34d8b3414e9b
Arthur Mattos <arsmmd@hotmail.com>
2021-02-06 19:01:32 -0300

    Updating NetReflector Libraries and adding new methods to compress files in SDK.

--- SDK/Files/Compression.cs
+++ SDK/Files/Compression.cs
@@ -13,7 +13,7 @@ namespace SoftmakeAll.SDK.Files
 
       System.Collections.Generic.Dictionary<System.String, System.String> FilesDictionary = new System.Collections.Generic.Dictionary<System.String, System.String>();
       foreach (System.String File in Files)
-        FilesDictionary.Add(File, File);
+        FilesDictionary.Add(File, new System.IO.FileInfo(File).Name);
 
       return SoftmakeAll.SDK.Files.Compression.CreateZipArchive(FilesDictionary);
     }
@@ -26,7 +26,35 @@ namespace SoftmakeAll.SDK.Files
       {
         using (System.IO.Compression.ZipArchive ZipArchive = new System.IO.Compression.ZipArchive(MemoryStream, System.IO.Compression.ZipArchiveMode.Create, false))
           foreach (System.Collections.Generic.KeyValuePair<System.String, System.String> File in Files)
-            ZipArchive.CreateEntryFromFile(File.Key, File.Value);
+            ZipArchive.CreateEntryFromFile(File.Value, File.Key);
+        return MemoryStream.ToArray();
+      }
+    }
+    public static System.Byte[] CreateZipArchive(System.Collections.Generic.Dictionary<System.String, System.Byte[]> Files)
+    {
+      if ((Files == null) || (Files.Count == 0))
+        return null;
+
+      using (System.IO.MemoryStream MemoryStream = new System.IO.MemoryStream())
+      {
+        using (System.IO.Compression.ZipArchive ZipArchive = new System.IO.Compression.ZipArchive(MemoryStream, System.IO.Compression.ZipArchiveMode.Create, false))
+          foreach (System.Collections.Generic.KeyValuePair<System.String, System.Byte[]> File in Files)
+            using (System.IO.Stream Stream = ZipArchive.CreateEntry(File.Key).Open())
+              Stream.Write(File.Value, 0, File.Value.Length);
+        return MemoryStream.ToArray();
+      }
+    }
+    public static async System.Threading.Tasks.Task<System.Byte[]> CreateZipArchiveAsync(System.Collections.Generic.Dictionary<System.String, System.Byte[]> Files)
+    {
+      if ((Files == null) || (Files.Count == 0))
+        return null;
+
+      using (System.IO.MemoryStream MemoryStream = new System.IO.MemoryStream())
+      {
+        using (System.IO.Compression.ZipArchive ZipArchive = new System.IO.Compression.ZipArchive(MemoryStream, System.IO.Compression.ZipArchiveMode.Create, false))
+          foreach (System.Collections.Generic.KeyValuePair<System.String, System.Byte[]> File in Files)
+            using (System.IO.Stream Stream = ZipArchive.CreateEntry(File.Key).Open())
+              await Stream.WriteAsync(File.Value, 0, File.Value.Length);
         return MemoryStream.ToArray();
       }
     }
