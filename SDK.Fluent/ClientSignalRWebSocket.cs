4672e568b6f9fa8e05dc0e4765fd1a91c987d41b
Arthur Mattos <arsmmd@hotmail.com>
2021-05-23 04:17:10 -0300

    Removing Dependencies from SDK.Fluents projects; Renaming WebSocketClient.cs to ClientSignalRWebSocket on SDK.Fluent; Creating the bare WebSocket client class on SDK.Communication

--- SDK.Fluent/ClientSignalRWebSocket.cs
+++ SDK.Fluent/ClientSignalRWebSocket.cs
@@ -1,10 +1,9 @@
 ﻿using SoftmakeAll.SDK.Helpers.JSON.Extensions;
 using Microsoft.AspNetCore.SignalR.Client;
-using System.Linq;
 
 namespace SoftmakeAll.SDK.Fluent
 {
-  public sealed class SoftmakeWS
+  public sealed class ClientSignalRWebSocket
   {
     #region Fields
     private Microsoft.AspNetCore.SignalR.Client.HubConnection WSConnection;
@@ -13,7 +12,7 @@ namespace SoftmakeAll.SDK.Fluent
     #endregion
 
     #region Constructor
-    public SoftmakeWS() { }
+    public ClientSignalRWebSocket() { }
     #endregion
 
     #region Events and Actions
@@ -25,7 +24,7 @@ namespace SoftmakeAll.SDK.Fluent
     #endregion
 
     #region Properties
-    internal System.Boolean Connected => ((this.WSConnection != null) && (this.WSConnection.State == HubConnectionState.Connected));
+    internal System.Boolean Connected => ((this.WSConnection != null) && (this.WSConnection.State == Microsoft.AspNetCore.SignalR.Client.HubConnectionState.Connected));
     #endregion
 
     #region Methods
@@ -34,7 +33,7 @@ namespace SoftmakeAll.SDK.Fluent
       if (System.String.IsNullOrWhiteSpace(Authorization))
         return;
 
-      await this.DestroyAsync();
+      await this.DisposeAsync();
 
       this.WSConnection = new Microsoft.AspNetCore.SignalR.Client.HubConnectionBuilder()
        .WithUrl(SoftmakeAll.SDK.Fluent.SDKContext.WebSocketBaseAddress, o =>
@@ -50,6 +49,12 @@ namespace SoftmakeAll.SDK.Fluent
       this.WSConnection.Closed += this.WSConnection_Closed;
       this.WSConnection.Reconnecting += this.WSConnection_Reconnecting;
       this.WSConnection.Reconnected += this.WSConnection_Reconnected;
+      this.WSConnection.On<System.Text.Json.JsonElement>("_mReceived", Message =>
+      {
+        if (!(Message.IsValid())) return;
+        try { this.MessageReceived?.Invoke(null, Message); } catch { }
+        try { this.OnMessageReceivedAction?.Invoke(Message); } catch { }
+      });
 
       try
       {
@@ -58,17 +63,30 @@ namespace SoftmakeAll.SDK.Fluent
       }
       catch
       {
-        this.WSConnection = null;
+        await this.DisposeAsync();
       }
-
-      this.WSConnection.On<System.Text.Json.JsonElement>("_mReceived", JSONMessage =>
-      {
-        if (!(JSONMessage.IsValid())) return;
-        try { this.MessageReceived?.Invoke(null, JSONMessage); } catch { }
-        try { this.OnMessageReceivedAction?.Invoke(JSONMessage); } catch { }
-      });
-
     }
+    internal async System.Threading.Tasks.Task SendAsync(System.Text.Json.JsonElement Message) => await this.WSConnection.SendAsync("_mSended", Message);
+    internal async System.Threading.Tasks.ValueTask DisposeAsync()
+    {
+      if (this.WSConnection == null)
+        return;
+
+      this.WSConnection.Reconnecting -= this.WSConnection_Reconnecting;
+      this.WSConnection.Reconnected -= this.WSConnection_Reconnected;
+
+      try { await this.WSConnection.StopAsync(); } catch { }
+
+      try
+      {
+        this.WSConnection.Closed -= this.WSConnection_Closed;
+        await this.WSConnection.DisposeAsync();
+      }
+      catch { }
+    }
+    #endregion
+
+    #region Event Handlers
     private System.Threading.Tasks.Task InvokeConnectionStateChangedEvents(System.String Event, System.String Arguments, System.Exception Exception)
     {
       System.Text.Json.JsonElement Message = new { Event, Arguments, Exception?.Message }.ToJsonElement();
@@ -76,26 +94,6 @@ namespace SoftmakeAll.SDK.Fluent
       try { this.OnConnectionStateChangedAction?.Invoke(Message); } catch { }
       return System.Threading.Tasks.Task.CompletedTask;
     }
-    internal async System.Threading.Tasks.Task DestroyAsync()
-    {
-      if (this.WSConnection != null)
-      {
-        this.WSConnection.Reconnecting -= this.WSConnection_Reconnecting;
-        this.WSConnection.Reconnected -= this.WSConnection_Reconnected;
-
-        try
-        {
-          await this.WSConnection.StopAsync();
-          this.WSConnection.Closed -= this.WSConnection_Closed;
-
-          await this.WSConnection.DisposeAsync();
-        }
-        catch { }
-      }
-    }
-    #endregion
-
-    #region Event Handlers
     private System.Threading.Tasks.Task WSConnection_Reconnected(System.String Arguments) => this.InvokeConnectionStateChangedEvents("Reconnected", Arguments, null);
     private System.Threading.Tasks.Task WSConnection_Reconnecting(System.Exception Exception) => this.InvokeConnectionStateChangedEvents("Reconnecting", null, Exception);
     private System.Threading.Tasks.Task WSConnection_Closed(System.Exception Exception) => this.InvokeConnectionStateChangedEvents("Closed", null, Exception);
---
+++ SDK/Communication/ClientWebSocket.cs
@@ -0,0 +1,193 @@
+﻿using System.Linq;
+
+namespace SoftmakeAll.SDK.Communication
+{
+  public class ClientWebSocket
+  {
+    #region Fields
+    private readonly System.String URL;
+    private readonly System.TimeSpan PingInterval;
+    private readonly System.Boolean AutomaticReconnection;
+
+    private System.Net.WebSockets.ClientWebSocket WebSocket = new System.Net.WebSockets.ClientWebSocket();
+    private SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates _State;
+    private SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates _LastState;
+    private System.Boolean _IsReconnection = false;
+    #endregion
+
+    #region Constructor
+    public ClientWebSocket(System.String URL) : this(URL, System.TimeSpan.Zero, false) { }
+    public ClientWebSocket(System.String URL, System.TimeSpan PingInterval) : this(URL, PingInterval, true) { }
+    public ClientWebSocket(System.String URL, System.TimeSpan PingInterval, System.Boolean AutomaticReconnection)
+    {
+      this.URL = URL;
+      this.PingInterval = PingInterval;
+      this.AutomaticReconnection = AutomaticReconnection;
+      this._State = ConnectionStates.Disconnected;
+      this._LastState = ConnectionStates.Disconnected;
+    }
+    #endregion
+
+    #region Enums
+    public enum ConnectionStates
+    {
+      Disconnected = 0,
+      Connected = 1,
+      Connecting = 2,
+      Reconnecting = 3
+    }
+    #endregion
+
+    #region Functions and Actions
+    private System.Action<System.String> ReceiveMessageAction;
+    #endregion
+
+    #region Events
+    public event System.Func<System.Exception, System.Threading.Tasks.Task> Closed;
+    public event System.Func<System.String, System.Threading.Tasks.Task> Reconnected;
+    public event System.Func<System.Exception, System.Threading.Tasks.Task> Reconnecting;
+    #endregion
+
+    #region Properties
+    public SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates State
+    {
+      get
+      {
+        return this._State;
+      }
+      private set
+      {
+        this._LastState = this.State;
+        this._State = value;
+      }
+    }
+    #endregion
+
+    #region Methods
+    private void Reconnect(System.Object State)
+    {
+      if (this.WebSocket.State == System.Net.WebSockets.WebSocketState.Open)
+      {
+        this.SendAsync($"{{ \"ping\": \"{System.DateTimeOffset.UtcNow}\" }}").ConfigureAwait(false);
+        return;
+      }
+
+      if (this.WebSocket.State == System.Net.WebSockets.WebSocketState.Connecting)
+        return;
+
+      if (this._LastState != this._State)
+      {
+        this.State = SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates.Disconnected;
+        this.Closed?.Invoke(this.WebSocket.CloseStatus == System.Net.WebSockets.WebSocketCloseStatus.NormalClosure || this.WebSocket.CloseStatusDescription == null ? null : new System.Exception(this.WebSocket.CloseStatusDescription));
+      }
+
+      if ((!(this.AutomaticReconnection)) || (this.WebSocket.CloseStatus == System.Net.WebSockets.WebSocketCloseStatus.NormalClosure))
+        return;
+
+      this.WebSocket.Abort();
+      this.WebSocket.Dispose();
+      this.WebSocket = new System.Net.WebSockets.ClientWebSocket();
+      try { this.StartAsync().ConfigureAwait(false); } catch { }
+    }
+    public async System.Threading.Tasks.Task StartAsync(System.Threading.CancellationToken CancellationToken = default)
+    {
+      switch (this.WebSocket.State)
+      {
+        case System.Net.WebSockets.WebSocketState.Connecting:
+        case System.Net.WebSockets.WebSocketState.Open:
+          return;
+        default:
+          break;
+      }
+
+      if (!(this._IsReconnection))
+        this.State = SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates.Connecting;
+      else
+      {
+        this.Reconnecting?.Invoke(null);
+        this.State = SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates.Reconnecting;
+      }
+
+      try
+      {
+        await this.WebSocket.ConnectAsync(new System.Uri(this.URL), CancellationToken);
+      }
+      catch
+      {
+        this.State = SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates.Disconnected;
+        throw;
+      }
+
+      if (this.WebSocket.State != System.Net.WebSockets.WebSocketState.Open)
+      {
+        this.State = SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates.Disconnected;
+        return;
+      }
+
+      this.State = SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates.Connected;
+      if (this._IsReconnection)
+        this.Reconnected?.Invoke(null);
+
+      _ = System.Threading.Tasks.Task.Run(async () =>
+      {
+        const System.Int16 BufferSize = 512;
+        System.Net.WebSockets.WebSocketReceiveResult Result;
+        do
+        {
+          System.Collections.Generic.IEnumerable<System.Byte> Data = new System.Byte[] { };
+
+          do
+          {
+            System.Byte[] Buffer = new System.Byte[BufferSize];
+            Result = await this.WebSocket.ReceiveAsync(new System.ArraySegment<System.Byte>(Buffer), System.Threading.CancellationToken.None);
+            if (Result.MessageType == System.Net.WebSockets.WebSocketMessageType.Text)
+              Data = Data.Concat(Buffer);
+          }
+          while (!(Result.EndOfMessage));
+
+          if ((Result.MessageType == System.Net.WebSockets.WebSocketMessageType.Text) && (this.ReceiveMessageAction != null))
+          {
+            this.ReceiveMessageAction(System.Text.Encoding.UTF8.GetString(Data.SkipLast(BufferSize - Result.Count).ToArray()));
+          }
+          else if (Result.MessageType == System.Net.WebSockets.WebSocketMessageType.Close)
+          {
+            this.State = SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates.Disconnected;
+            this.Closed?.Invoke(Result.CloseStatus == System.Net.WebSockets.WebSocketCloseStatus.NormalClosure || Result.CloseStatusDescription == null ? null : new System.Exception(Result.CloseStatusDescription));
+          }
+        }
+        while (this.WebSocket.State == System.Net.WebSockets.WebSocketState.Open);
+      });
+
+      if ((!(this._IsReconnection)) && (this.PingInterval != System.TimeSpan.Zero))
+        _ = new System.Threading.Timer(this.Reconnect, null, System.TimeSpan.Zero, this.PingInterval);
+
+      this._IsReconnection = true;
+    }
+    public async System.Threading.Tasks.Task SendAsync(System.String Message, System.Threading.CancellationToken CancellationToken = default)
+    {
+      if (this.WebSocket.State == System.Net.WebSockets.WebSocketState.Open)
+        await this.WebSocket.SendAsync(new System.ArraySegment<System.Byte>(System.Text.Encoding.UTF8.GetBytes(Message)), System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken);
+    }
+    public void On(System.Action<System.String> ReceiveMessageAction) => this.ReceiveMessageAction = ReceiveMessageAction;
+    public async System.Threading.Tasks.Task StopAsync(System.Threading.CancellationToken CancellationToken = default)
+    {
+      switch (this.WebSocket.State)
+      {
+        case System.Net.WebSockets.WebSocketState.Open:
+          {
+            await this.WebSocket.CloseAsync(System.Net.WebSockets.WebSocketCloseStatus.NormalClosure, null, CancellationToken);
+            this.State = SoftmakeAll.SDK.Communication.ClientWebSocket.ConnectionStates.Disconnected;
+            break;
+          }
+        default:
+          break;
+      }
+    }
+    public async System.Threading.Tasks.ValueTask DisposeAsync()
+    {
+      await this.StopAsync(new System.Threading.CancellationToken(true));
+      this.WebSocket.Dispose();
+    }
+    #endregion
+  }
+}
